9c007d249132e5cbec54f955ffc72d872276c65f
starmyn <oiasmyn25@gmail.com>
2024-05-09 15:58:24 -0300

    Update exercicio_1.py

--- condicionais/exercicio_1.py
+++ condicionais/exercicio_1.py
@@ -5,15 +5,37 @@ import urllib.request
 cep = input('Digite o CEP:')
 
 # Construir na URL de consulta
-url = f"https://viacep.com.br/ws/{cep}json/"
+url = f"https://viacep.com.br/ws/{cep}/json/"
 
 try:
     # Fazer a requisição
     response = urllib.request.urlopen(url)
 
-    #Ler o conteudo da resposta
+    # Ler o conteudo da resposta
     data = response.read().decode('utf-8')
-    # Teste...
-    print(data)
+    
+    #   Converter JSON em dicionario Python
+    endereco = json.loads(data)
+
+    # Verificar se a consulta foi bem-sucedida 
+    if endereco.get('erro'):
+        print("CEP ão encontrado")
+    else:
+        # Armazenar informaçoes em variáveis
+        logradouro = endereco['logradouro']
+        complemento = endereco['complemento']
+        bairro = endereco['bairro']
+        cidade = endereco['localidade']
+        estado = endereco['uf']
+    
+        # Exibir informaçoes do endereço na tela
+        print(f"Logradouro: {logradouro}")
+        print(f"Completamento: {complemento}")
+        print(f"Bairro:{bairro}")
+        print(f"Cidade:{cidade}")
+        print(f"Estado:{estado}")
+    
+    # Fechar a conexão
+    response.close()
 except Exception as e:
     print(f"Erro: {e}")
