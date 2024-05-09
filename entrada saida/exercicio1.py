01e3c0929fa71acd0394d513bbfbe438631f5cf1
starmyn <oiasmyn25@gmail.com>
2024-05-09 09:28:10 -0300

    exercicio1.py

---
+++ entrada saida/exercicio1.py
@@ -0,0 +1,7 @@
+#soma e produto entre 2 números inteiros
+numero_1=int(input('digite o primeiro número(inteiro):'))
+numero_2=int(input('digite o segundo número (inteiro):'))
+soma=numero_1+numero_2 
+print(f'o resultado da soma é {soma}')
+produto=numero_1*numero_2
+print(f'o produtos dos números é {produto}')
